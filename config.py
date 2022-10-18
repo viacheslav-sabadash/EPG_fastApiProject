29799778504deba21bcda4a12b25dcf978ce8efa
viacheslav.sabadash <viacheslav.sabadash@wecandevelopit.com>
2022-10-18 22:47:09 +0300

    fix regex for empty values

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "Calculator"
     hello_text = "Hello world"
-    val_regex: str = '^[0-9, /*+.-]+$'
+    val_regex: str = '^[0-9, /*+.-]*$'
 
     class Config:
         env_file = ".env"
